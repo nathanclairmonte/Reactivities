0f0e0492e023dd6fb232fbdad09205a04c731ec2
Nathan <nathanclairmonte@outlook.com>
2020-05-18 14:41:25 -0400

    Initial Commit

---
+++ Domain/Value.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Value
+    {
+        // Id name will auto be used as PK
+        // int type will use auto gen Id's
+        // Will be using Code First in this course
+        public int Id { get; set; }
+        public string Name  { get; set; }
+    }
+}
